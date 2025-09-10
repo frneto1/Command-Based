d8f28e86b097f0df1817ca0f585a46a407d5d6b5
Francisco Neto <francisco.neto2903@gmail.com>
2025-09-10 15:31:42 -0300

    Update RobotContainer.java

--- robot/RobotContainer.java
+++ robot/RobotContainer.java
@@ -2,26 +2,29 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autonomous;
 import frc.robot.commands.Locomocao;
 import frc.robot.subsystems.Calcs;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
 
-public class RobotContainer {
-  public DriveSubsystem driveSubsystem = new DriveSubsystem();
-
+public class RobotContainer{
   private final Joystick bob = new Joystick (OperatorConstants.kDriverControllerPort);
-
+  public DriveSubsystem driveSubsystem = new DriveSubsystem();
   public Calcs calcs = new Calcs(bob);
 
+  public Autonomous auto = new Autonomous(driveSubsystem, calcs);
 
   public RobotContainer() {
     driveSubsystem.setDefaultCommand(new Locomocao(driveSubsystem, calcs, bob));
-
+    getAutonomous();
   }
-
   public void configureBindings() {
 
-      }
-
+  }
+  public Command getAutonomous(){
+    return auto;
+  }
+  
 }
