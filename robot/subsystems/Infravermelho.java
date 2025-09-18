6c107cf17e7a70ad64f8dfdaad63b563455eca3e
Francisco Neto <francisco.neto2903@gmail.com>
2025-09-18 16:42:58 -0300

    Update Infravermelho.java

--- robot/subsystems/Infravermelho.java
+++ robot/subsystems/Infravermelho.java
@@ -1,10 +1,11 @@
+
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class infravermelho extends SubsystemBase {
-    public DigitalInput sensor = new DigitalInput(0);
+public class Infravermelho extends SubsystemBase {
+    public DigitalInput sensor = new DigitalInput(4);
 
     public boolean objDetectado(){
         return !sensor.get();
