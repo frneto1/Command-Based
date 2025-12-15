7d08b52394a90cf9b6ec0bd21d0a515f3dde814e
Francisco Neto <francisco.neto2903@gmail.com>
2025-12-15 10:40:02 -0300

    Update RobotContainer.java

--- robot/RobotContainer.java
+++ robot/RobotContainer.java
@@ -6,13 +6,12 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autonomous;
 import frc.robot.commands.Locomocao;
-import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.Calcs;
+import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.EjectorSubsystem;
 import frc.robot.subsystems.Infravermelho;
 import frc.robot.subsystems.Limelight;
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -24,11 +23,12 @@ public class RobotContainer{
   public Infravermelho ir = new Infravermelho();
   public Calcs calcs = new Calcs(bob, roberto);
   public EjectorSubsystem ejector = new EjectorSubsystem();
+  public ClimbSubsystem climber = new ClimbSubsystem();
 
-  public Autonomous auto = new Autonomous(driveSubsystem, ir, limelight);
+  public Autonomous auto = new Autonomous(driveSubsystem, ir, limelight, ejector);
 
   public RobotContainer() {
-    driveSubsystem.setDefaultCommand(new Locomocao(driveSubsystem, calcs, bob, roberto, ejector));
+    driveSubsystem.setDefaultCommand(new Locomocao(driveSubsystem, calcs, bob, roberto, ejector, ir, climber));
     getAutonomous();
   }
   public void configureBindings() {
